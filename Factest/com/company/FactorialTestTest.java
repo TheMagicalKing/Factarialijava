55bb71ba2655a39e540f9570196594577c94ec7b
sune <sune1309@edu.easj.dk>
2019-09-24 15:04:43 +0200

    Initial commit

---
+++ Factest/com/company/FactorialTestTest.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import junit.framework.TestCase;
+
+public class FactorialTestTest extends TestCase {
+    public void  testbergn() {
+        int resultat = FactorialTest.
+    }
+
+
+
+
+}
---
+++ src/com/company/FactorialTest.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class FactorialTest {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 1; i <=200 ; i = i * 5) {
+            System.out.println(i);
+
+        }
+
+    }
+}
